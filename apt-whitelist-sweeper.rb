b58e87aad8896edadc9e5fb5a0b89753ef579ebe
Hiro Asari <asari.ruby@gmail.com>
2015-07-19 09:56:31 -0400

    Beef up apt-whitelist-sweeper script
    (Almost ready to *really* use it)

--- apt-whitelist-sweeper.rb
+++ apt-whitelist-sweeper.rb
@@ -4,6 +4,19 @@ require 'json'
 require 'faraday'
 require 'uri'
 
+def next_link_in_headers(headers)
+  # "<https://api.github.com/repositories/1420493/issues?labels=apt-whitelist&page=2>; rel=\"next\", <https://api.github.com/repositories/1420493/issues?labels=apt-whitelist&page=6>; rel=\"last\""
+  next_link_text = headers['link'].split(',').find { |l| l.end_with? 'rel="next"' }
+  if next_link_text
+    match_data = next_link_text.match(/<(?<next>[^>]+)>/)
+    if match_data
+      match_data[:next]
+    end
+  end
+end
+
+run_it = false
+
 github_api = "https://api.github.com"
 travis_api = 'https://api.travis-ci.org'
 
@@ -19,75 +32,88 @@ travis_conn = Faraday.new(:url => travis_api) do |faraday|
   faraday.adapter Faraday.default_adapter
 end
 
-params = { labels: 'apt-whitelist' }
 repo   = 'travis-ci/travis-ci'
+next_page_url = "/repos/#{repo}/issues"
 
-response = conn.get "/repos/#{repo}/issues", params
-
-tickets = JSON.parse(response.body)
-
-# tickets.each do |t|
-tickets.take(1).each do |t|
-  unless match_data = /(?i:apt whitelist request for (?<package_name>[-\.\w]+))/.match(t['title'])
-    puts "#{t['title']} is ambiguous"
-    next
+loop do
+  response = conn.get do |req|
+    req.url next_page_url
+    req.headers['Content-Type'] = 'application/json'
+    req.headers['Authorization'] = "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
+    req.params['labels'] = 'apt-whitelist'
   end
 
-  pkg = match_data[:package_name]
-  issue_number = t["url"].split('/').last
+  tickets = JSON.parse(response.body)
 
-  labels = t['labels']
-  if labels.any? { |l| l['name'] == 'apt-source-whitelist' }
-    puts "#{pkg} needs source whitelisting"
-    next
-  end
+  tickets.each do |t|
+    issue_number = t["url"].split('/').last
 
-  if labels.any? { |l| l['name'] == 'apt-whitelist-check-run' }
-    puts "#{pkg} has been checked already"
-    next
-  end
+    unless match_data = /\A(?i:apt whitelist request for (?<package_name>\S+))\z/.match(t['title'])
+      puts "'#{t['title']}' is ambiguous; #{issue_number}"
+      next
+    end
 
-  puts "#{t['title']}, #{issue_number}"
+    pkg = match_data[:package_name]
 
-  message = "Run apt-source-whitelist check for #{pkg}; #{Time.now.utc.strftime('%Y-%m-%d-%H-%M-%S')}\n\nSee travis-ci/travis-ci##{issue_number}"
+    labels = t['labels'].tap {|x| puts "labels: #{x}"}
+    if labels.any? { |l| l['name'] == 'apt-source-whitelist' }
+      puts "#{pkg} needs source whitelisting"
+      next
+    end
 
-  payload = {
-    "request"=> {
-      "message" => message,
-      "branch"  => 'master',
-      "config"  => {
-        "env" => {
-          "global" => ["PACKAGE=#{pkg}"]
+    if labels.any? { |l| l['name'] == 'apt-whitelist-check-run' }
+      puts "#{pkg} has been checked already"
+      next
+    end
+
+    puts "#{t['title']}, #{issue_number}"
+
+    message = "Run apt-source-whitelist check for #{pkg}; #{Time.now.utc.strftime('%Y-%m-%d-%H-%M-%S')}\n\nSee travis-ci/travis-ci##{issue_number}"
+
+    payload = {
+      "request"=> {
+        "message" => message,
+        "branch"  => 'default',
+        "config"  => {
+          "env" => {
+            "global" => ["PACKAGE=#{pkg}"]
+          }
         }
       }
     }
-  }
 
-  travis_response = travis_conn.post do |req|
-    req.url "/repo/BanzaiMan%2Fapt-whitelist-checker/requests"
-    req.headers['Content-Type'] = 'application/json'
-    req.headers['Travis-API-Version'] = '3'
-    req.headers['Authorization'] = "token #{ENV["TRAVIS_TOKEN"]}"
-    req.body = payload.to_json
-  end
+    puts "going to run test on #{pkg}"
 
-  if travis_response.success?
-    # build request was accepted
-    comment = "Run basic check to see if the package conatins setuid/setgid/seteuid calls. See URL"
+    if run_it
+      travis_response = travis_conn.post do |req|
+        req.url "/repo/BanzaiMan%2Fapt-whitelist-checker/requests"
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['Travis-API-Version'] = '3'
+        req.headers['Authorization'] = "token #{ENV["TRAVIS_TOKEN"]}"
+        req.body = payload.to_json
+      end
 
-    conn.post do |req|
-      req.url "/repos/#{repo}/issues/#{issue_number}/comments"
-      req.headers['Content-Type'] = 'application/json'
-      req.headers['Authorization'] = "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
-      req.body = { "body": comment }
+      if travis_response.success?
+        # build request was accepted
+        comment = "Automated running a basic check to see if the package conatins suspicious setuid/setgid/seteuid calls."
+
+        conn.post do |req|
+          req.url "/repos/#{repo}/issues/#{issue_number}/comments"
+          req.headers['Content-Type'] = 'application/json'
+          req.headers['Authorization'] = "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
+          req.body = { "body" => comment }
+        end
+
+        conn.post do |req|
+          req.url "/repos/#{repo}/issues/#{issue_number}/labels"
+          req.headers['Content-Type'] = 'application/json'
+          req.headers['Authorization'] = "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
+          req.body = [ 'apt-whitelist-check-run' ]
+        end
+      end
     end
 
-    conn.post do |req|
-      req.url "/repos/#{repo}/issues/#{issue_number}/labels"
-      req.headers['Content-Type'] = 'application/json'
-      req.headers['Authorization'] = "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
-      req.body = [ 'apt-whitelist-check-run' ]
-    end
   end
 
+  break unless next_page_url = next_link_in_headers(response.headers).tap {|x| puts x}
 end
