514d4795774f070a41a2f2554c53d036380dbaf5
Hiro Asari <asari.ruby@gmail.com>
2015-07-20 22:12:03 -0400

    Manage GitHub issues comments from this repo
    Instead of commenting on the GitHub issue from outside entity that
    initiates the build, we post comments from this repository.
    
    The advantage is that, when we receive the build request, we
    can receive the APT package name and the GitHub issue number.
    
    The basic workflow is like this:
    
    * Run our check on a docker container on the VM, then depending
    on the result, we exit the ssh session.
    * Based on the exit status, we create a PR or post comments

--- build.sh
+++ build.sh
@@ -7,11 +7,29 @@ ANSI_GREEN="\033[32;1m"
 ANSI_RESET="\033[0m"
 ANSI_CLEAR="\033[0K"
 
+EXIT_SOURCE_NOT_FOUND=1
+EXIT_SOURCE_HAS_SETUID=2
+
 export DEBIAN_FRONTEND=noninteractive
+echo "Fetching apt-source-whitelist data"
 wget https://raw.githubusercontent.com/travis-ci/apt-source-whitelist/master/ubuntu.json
-ruby -rjson -e 'json=JSON.parse(File.read("ubuntu.json")); json.each {|src| system "curl -sSL #{src["key_url"].untaint.inspect} | sudo apt-key add -" if src["key_url"]; system "sudo -E apt-add-repository -y #{src["sourceline"].untaint.inspect}" }'
+echo "Applying apt-source-whitelist data"
+ruby -rjson -e 'json=JSON.parse(File.read("ubuntu.json")); json.each {|src| puts "Adding #{src["key_url"].untaint.inspect}"; system "curl -sSL #{src["key_url"].untaint.inspect} | sudo apt-key add - &>/dev/null" || puts("Failed") if src["key_url"]; puts "Adding repository #{src["sourceline"].untaint.inspect}"; system("sudo -E apt-add-repository -y #{src["sourceline"].untaint.inspect}) || puts("failed")" }'
 mkdir -p /var/tmp/deb-sources
 cd /var/tmp/deb-sources
 sudo apt-get update -qq
-apt-get source ${PKG}
-grep -R -i -E 'set(uid|euid|gid)' . 2>/dev/null || echo -e "\n\n${ANSI_GREEN}no setuid bits found${ANSI_CLEAR}\n\n"
+
+echo "Fetching source package for ${PKG}"
+apt-get source ${PKG} &> apt-get-result.log
+
+if egrep 'Unable to find a source package for' apt-get-result.log 2>/dev/null; then
+	exit $EXIT_SOURCE_NOT_FOUND
+fi
+
+if find . ! -name install.sh -a -type f -exec grep -i -H -C5 -E --color 'set(uid|euid|gid)' {} \; 2>/dev/null; then
+	echo -e "${ANSI_GREEN}No setuid bits found${ANSI_RESET}"
+	exit 0
+else
+	echo -e "${ANSI_RED}Suspicious bits found${ANSI_RESET}"
+	exit $EXIT_SOURCE_HAS_SETUID
+fi
---
+++ run_test.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env bash -e
+
+ANSI_RED="\033[31;1m"
+ANSI_GREEN="\033[32;1m"
+ANSI_RESET="\033[0m"
+ANSI_CLEAR="\033[0K"
+
+EXIT_SUCCSS=0
+EXIT_SOURCE_NOT_FOUND=1
+EXIT_SOURCE_HAS_SETUID=2
+
+BUILD_URL="https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}"
+GITHUB_ISSUE_URL="https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${ISSUE_NUMBER}"
+
+sshpass -p travis scp $SSH_OPTS build.sh travis@$(< docker_ip_address):.
+sshpass -p travis ssh -n -t -t $SSH_OPTS travis@$(< docker_ip_address) "bash build.sh ${PACKAGE}"
+
+case $? in
+	$EXIT_SUCCSS)
+		BRANCH="apt-package-whitelist-test-${ISSUE_NUMBER}"
+		pushd ../apt-package-whitelist
+		git checkout -b
+		env TICKET=${ISSUE_NUMBER} make resolve
+		git push origin $BRANCH
+		popd
+		COMMENT="Ran tests and found no setuid bits.\n\nSee${BUILD_URL}"
+		curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
+			-d "{\"title\":\"Pull request for ${PACKAGE}\",\"body\":\"${COMMENT}\",\"head\":\"${BRANCH}\",\"base\":\"master\"}" \
+			https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls
+		;;
+	$EXIT_SOURCE_HAS_SETUID)
+		COMMENT="Ran tests and found setuid bits.\n\nSee${BUILD_URL}"
+		curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
+			-d "{\"body\":\"${COMMENT}\"}" \
+			${GITHUB_ISSUE_URL}/comments
+		;;
+	$EXIT_SOURCE_NOT_FOUND)
+		COMMENT="Ran tests, but could not found source package. Either the source package for ${PACKAGE} does not exist, or needs an APT source.\n\nSee${BUILD_URL}"
+		curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
+			-d "{\"body\":\"${COMMENT}\"}" \
+			${GITHUB_ISSUE_URL}/comments
+		curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
+			-d "[\"apt-source-whitelist\"]" \
+			${GITHUB_ISSUE_URL}/labels
+		;;
+	*)
+		echo "${ANSI_RED}Something unexpected happened${ANSI_RESET}"
+		;;
+esac
